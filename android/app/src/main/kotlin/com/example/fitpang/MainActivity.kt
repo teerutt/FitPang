4eeeca1a93fe0b94ace1206cb9a2b9e5317f47c8
teerutt <tee008.ph@gmail.com>
2023-10-05 17:19:31 +0700

    onboarding

---
+++ android/app/src/main/kotlin/com/example/fitpang/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fitpang
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/onboarding/onboarding_view.dart
@@ -0,0 +1,126 @@
+import 'package:fitpang/common_widget/onboarding_page.dart';
+import 'package:flutter/material.dart';
+import '../../common/color_extension.dart';
+
+class OnBoardingView extends StatefulWidget {
+  const OnBoardingView({super.key});
+
+  @override
+  State<OnBoardingView> createState() => _OnBoardingViewState();
+}
+
+class _OnBoardingViewState extends State<OnBoardingView> {
+  int selectPage = 0;
+  PageController controller = PageController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    controller.addListener(() {
+        selectPage = controller.page?.round() ?? 0;
+
+      setState(() {
+        
+      });
+    });
+  }
+
+  List pageArr = [
+    {
+      "title": "Track Your Goal",
+      "subtitle":
+          "Don't worry if you have trouble determining your goals, We can help you determine your goals and track your goals",
+      "image": "assets/img/onboarding1.png"
+    },
+    {
+      "title": "Get Burn",
+      "subtitle":
+          "Let’s keep burning, to achive yours goals, it hurts only temporarily, if you give up now you will be in pain forever",
+      "image": "assets/img/onboarding2.png"
+    },
+    {
+      "title": "Eat Well",
+      "subtitle":
+          "Let's start a healthy lifestyle with us, we can determine your diet every day. healthy eating is fun",
+      "image": "assets/img/onboarding3.png"
+    },
+    {
+      "title": "Improve Sleep\nQuality",
+      "subtitle":
+          "Improve the quality of your sleep with us, good quality sleep can bring a good mood in the morning",
+      "image": "assets/img/onboarding4.png"
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: TColor.black,
+      body: Stack(
+        alignment: Alignment.bottomRight,
+        children: [
+          PageView.builder(
+              controller: controller,
+              itemCount: pageArr.length,
+              itemBuilder: (context, index) {
+                var pObj = pageArr[index] as Map? ?? {};
+                return OnBoardingPage(pObj: pObj) ;
+              }),
+
+          SizedBox(
+            width: 120,
+            height: 120,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+
+                SizedBox(
+                  width: 70,
+                  height: 70,
+                  child: CircularProgressIndicator(
+                    color: TColor.primaryColor1,
+                    value: (selectPage + 1) / 4 ,
+                    strokeWidth: 2,
+                  ),
+                ),
+
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(color: TColor.primaryColor1, borderRadius: BorderRadius.circular(35)),
+                  child: IconButton(icon: Icon( Icons.navigate_next, color: TColor.black, ), onPressed: (){
+          
+                      if(selectPage < 3) {
+          
+                         selectPage = selectPage + 1;
+
+                        // controller.animateToPage(selectPage, duration: const Duration(milliseconds: 600), curve: Curves.bounceInOut);
+                        
+                        controller.jumpToPage(selectPage);
+                        
+                          setState(() {
+                            
+                          });
+          
+                      }else{
+                        // Open Welcome Screen
+                        print("Open Welcome Screen");
+                        // Navigator.push(context, MaterialPageRoute(builder: (context) => const SignUpView() ));
+                      }
+                      
+                  },),
+                ),
+
+                
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
