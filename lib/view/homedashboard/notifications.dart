ccbd3b255560df37e40d152b074bbed5f01148a9
leequeRz <92385972+leequeRz@users.noreply.github.com>
2023-11-09 00:39:45 +0700

    add noti can delete

--- lib/view/homedashboard/notifications.dart
+++ lib/view/homedashboard/notifications.dart
@@ -10,83 +10,51 @@ class Notifications extends StatefulWidget {
 }
 
 class _NotificationsState extends State<Notifications> {
-  bool isBarVisible = false;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // To make the bar visible, you can use a Timer or some other trigger.
-    // For demonstration, we will toggle it after a delay.
-    Future.delayed(Duration(seconds: 1), () {
-      setState(() {
-        isBarVisible = true;
-      });
-    });
-  }
+  List<int> items = List<int>.generate(100, (int index) => index);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: TColor.white,
       body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  "Notifications",
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 10),
-                Container(
-                  color: Colors.black, // Color of the SizedBox
-                  height: 3, // Adjust the height as needed
-                ),
-                SizedBox(height: 15), // Add some spacing
-                Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: TColor.primaryG,
-                    ), // Background color
-                    borderRadius: BorderRadius.all(
-                        Radius.circular(20.0)), // Border radius
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(25.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              "Congratulations",
-                              style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black, // Text color
-                              ),
-                            ),
-                            // Text("35% your daily challenge completed")
-                          ],
-                        ),
-                        Text(
-                          DateFormat('jm').format(DateTime.now()),
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black, // Text color
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+        child: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.center, // Center the content vertically
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 25.0, horizontal: 25.0),
+              child: Text(
+                "Notifications",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: items.length,
+                // padding: const EdgeInsets.symmetric(vertical: 10.0),
+                itemBuilder: (BuildContext context, int index) {
+                  return Dismissible(
+                    background: Container(
+                      color: Colors.red,
+                      child: const Icon(Icons.delete),
+                    ),
+                    key: ValueKey<int>(items[index]),
+                    onDismissed: (DismissDirection direction) {
+                      setState(() {
+                        items.removeAt(index);
+                      });
+                    },
+                    child: ListTile(
+                      title: Text(
+                        'Item ${items[index]}',
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
